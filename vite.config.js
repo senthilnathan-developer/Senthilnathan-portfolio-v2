63557c831adc56d74d8d062d7ce36177c0692cff
Senthilnathan-es02 <eswarensenthil@gmail.com>
2025-07-22 13:40:41 +0530

    portfolio v2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  base: '/Senthilnathan-portfolio-v2-/', // 👈 VERY IMPORTANT
+  base: "/Senthilnathan-portfolio-v2-/",
   plugins: [react()],
-})
+});
+
