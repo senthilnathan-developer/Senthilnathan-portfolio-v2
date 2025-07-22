fb301c656367e7d48c0682bb944883264d90bf5d
Senthilnathan-es02 <eswarensenthil@gmail.com>
2025-07-22 13:49:25 +0530

    portfolio v2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({
-  base: "/Senthilnathan-portfolio-v2-/",
-  plugins: [react()],
-});
 
+export default defineConfig({
+  base: '/Senthilnathan-portfolio-v2-/', // 👈 this should match your repo name
+  plugins: [react()],
+})
